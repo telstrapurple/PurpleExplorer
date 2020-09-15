c7cfcb632db1544bc7f6b4a0917d0e994e336fce
Juin <yi.juin.lim@purple.telstra.com>
2020-09-15 15:15:52 +1000

    remove unused BtnConnectCommand()

--- PurpleExplorer/ViewModels/ConnectionStringWindowViewModel.cs
+++ PurpleExplorer/ViewModels/ConnectionStringWindowViewModel.cs
@@ -1,18 +1,7 @@
-﻿using PurpleExplorer.Helpers;
-using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace PurpleExplorer.ViewModels
+﻿namespace PurpleExplorer.ViewModels
 {
     public class ConnectionStringWindowViewModel : ViewModelBase
     {
         public string ConnectionString { get; set; }
-
-        public async void BtnConnectCommand()
-        {
-            if (string.IsNullOrEmpty(ConnectionString))
-                await MessageBoxHelper.ShowError(MessageBox.Avalonia.Enums.ButtonEnum.OkCancel, "Error", "Please enter a service bus connection string.");
-        }
     }
 }
