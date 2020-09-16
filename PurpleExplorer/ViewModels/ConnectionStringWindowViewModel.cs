8aaedd53bfc00a7bff1e870f027e26d799520a84
Salman Ali Banani <salman.banani@purple.telstra.com>
2020-09-16 15:22:14 +1000

    If a dialog is close by clicking x, nothing will happen

--- PurpleExplorer/ViewModels/ConnectionStringWindowViewModel.cs
+++ PurpleExplorer/ViewModels/ConnectionStringWindowViewModel.cs
@@ -1,7 +1,9 @@
 ﻿namespace PurpleExplorer.ViewModels
 {
-    public class ConnectionStringWindowViewModel : ViewModelBase
+    public class ConnectionStringWindowViewModel : DialogViewModelBase
     {
         public string ConnectionString { get; set; }
+
+        public ConnectionStringWindowViewModel() : base() { }
     }
 }
---
+++ PurpleExplorer/ViewModels/DialogViewModelBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PurpleExplorer.ViewModels
+{
+    public class DialogViewModelBase : ViewModelBase 
+    {
+        public bool Cancel { get; set; }
+        
+        public DialogViewModelBase()
+        {
+            this.Cancel = true;
+        }
+    }
+}
