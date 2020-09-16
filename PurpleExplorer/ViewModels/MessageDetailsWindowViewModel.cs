b9329ab341f7d18f210c4f08099bbf3526bd7005
Salman Ali Banani <salman.banani@purple.telstra.com>
2020-09-16 10:39:36 +1000

    Message details window work in progress #26416

---
+++ PurpleExplorer/ViewModels/MessageDetailsWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿using PurpleExplorer.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PurpleExplorer.ViewModels
+{
+    public class MessageDetailsWindowViewModel : ViewModelBase
+    {
+        public Message Message { get; set; }
+    }
+}
