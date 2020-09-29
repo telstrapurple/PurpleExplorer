d083ef2d3c4aa8a8b98a2d9558714f31cddef22b
yj <yjlim214@gmail.com>
2020-09-29 14:29:56 +1000

    reverted icons back to XAML files

--- PurpleExplorer/Helpers/ModalWindowHelper.cs
+++ PurpleExplorer/Helpers/ModalWindowHelper.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Reflection;
-using Avalonia;
+﻿using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
 using PurpleExplorer.ViewModels;
 using System.Threading.Tasks;
-using Avalonia.Media.Imaging;
-using Avalonia.Platform;
 
 namespace PurpleExplorer.Helpers
 {
@@ -19,12 +15,6 @@ namespace PurpleExplorer.Helpers
 
             T window = new T();
             window.DataContext = viewModel;
-            
-            var assets = AvaloniaLocator.Current.GetService<IAssetLoader>();
-            string assemblyName = Assembly.GetEntryAssembly()?.GetName().Name;
-            var rawUri = "/Assets/avalonia-logo.ico";
-            var bitmap = new Bitmap(assets.Open(new Uri($"avares://{assemblyName}{rawUri}")));
-            window.Icon = new WindowIcon(bitmap);
 
             await window.ShowDialog(mainWindow);
             return window.DataContext as U;
