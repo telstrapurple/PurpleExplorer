e46bf9fdf804250b9cccfee4fcde859d1ad3601e
Luke Warren <lukejkw@gmail.com>
2023-01-27 16:14:15 +0200

    fix: connection string invalid for topic limit > 100

--- PurpleExplorer/Helpers/ITopicHelper.cs
+++ PurpleExplorer/Helpers/ITopicHelper.cs
@@ -10,9 +10,6 @@ public interface ITopicHelper
 {
     public Task<NamespaceInfo> GetNamespaceInfo(ServiceBusConnectionString connectionString);
     public Task<IList<ServiceBusTopic>> GetTopicsAndSubscriptions(ServiceBusConnectionString connectionString);
-    public Task<ServiceBusTopic> GetTopic(ServiceBusConnectionString connectionString, string topicPath, bool retrieveSubscriptions);
-    public Task<IList<ServiceBusSubscription>> GetSubscriptions(ServiceBusConnectionString connectionString, string topicPath);
-    public Task<ServiceBusSubscription> GetSubscription(ServiceBusConnectionString connectionString, string topicPath, string subscriptionName);
     public Task<IList<Message>> GetDlqMessages(ServiceBusConnectionString connectionString, string topic, string subscription);
     public Task<IList<Models.Message>> GetMessagesBySubscription(ServiceBusConnectionString connectionString, string topicName, string subscriptionName);
     public Task SendMessage(ServiceBusConnectionString connectionString, string topicPath, string content);
